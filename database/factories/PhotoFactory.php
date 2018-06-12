1cd2127e35baa9f85fdb05705295eb722fa695dc
vagrant <vagrant@homestead>
2018-06-12 03:22:12 +0000

    Seeding photos and users data

--- database/factories/PhotoFactory.php
+++ database/factories/PhotoFactory.php
@@ -3,7 +3,21 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Photo::class, function (Faker $faker) {
+
+    $sentence = $faker->sentence();
+
+    // random datetime in one month
+    $updated_at = $faker->dateTimeThisMonth();
+    // 传参为生成最大时间不超过，创建时间永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
         // 'name' => $faker->name,
+        'title' => $sentence,
+        'year' => $faker->year(),
+        'month' => $faker->month(),
+        'description' => $faker->text(),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
--- database/seeds/PhotosTableSeeder.php
+++ database/seeds/PhotosTableSeeder.php
@@ -2,17 +2,44 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Photo;
+use App\Models\User;
+use App\Models\Category;
 
 class PhotosTableSeeder extends Seeder
 {
     public function run()
     {
-        $photos = factory(Photo::class)->times(50)->make()->each(function ($photo, $index) {
-            if ($index == 0) {
-                // $photo->field = 'value';
-            }
+        // $photos = factory(Photo::class)->times(50)->make()->each(function ($photo, $index) {
+        //     if ($index == 0) {
+        //         // $photo->field = 'value';
+        //     }
+        // });
+
+        // Photo::insert($photos->toArray());
+
+        // 所有用户 ID 数组，如：[1,2,3,4]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组，如：[1,2,3,4]
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        // fetch Faker instance
+        $faker = app(Faker\Generator::class);
+
+        $photos = factory(Photo::class)
+                        ->times(100)
+                        ->make()
+                        ->each(function ($photo, $index)
+                            use ($user_ids, $category_ids, $faker)
+        {
+            // 从用户 ID 数组中随机取出一个并赋值
+            $photo->user_id = $faker->randomElement($user_ids);
+
+            // 话题分类，同上
+            $photo->category_id = $faker->randomElement($category_ids);
         });
 
+        // 将数据集合转换为数组，并插入到数据库中
         Photo::insert($photos->toArray());
     }
 
